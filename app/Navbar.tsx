7b1ec9b6c85fb4ca536d176e8f2544344bf5968b
Ujjwal Bhattarai <bhattaraiujjwal26@gmail.com>
2023-10-12 23:18:14 +0530

    setup prisma

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,22 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
-import {AiFillBug} from 'react-icons/ai'
+'use client'
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const Navbar = () => {
-    const links = [
-        {label: "Dashboard", href: "/"},
-        {label: "Issues", href: "/issues"}, 
-    ]
+  const currPath = usePathname();
+  console.log(currPath == '/');
+  
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
     <nav className="flex space-x-6 px-5 h-14 border-b mb-5 items-center">
-        <Link href="/"><AiFillBug/></Link>
-        <ul className='flex space-x-5'>
-            {links.map(item => {
-                return <li key={item.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'><Link href={item.href}>{item.label}</Link></li>
-            })}
-        </ul>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-5">
+        {links.map((item) => {
+          return (
+            <Link
+                key={item.href}
+              href={item.href}
+              className={classNames({
+                "text-zinc-950": item.href == currPath,
+                "text-zinc-500": item.href != currPath,
+                "hover: text-zinc-800 transition-colors": true,
+              })}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssuesPage= () => {
+  return (
+    <div>Issues page</div>
+  )
+}
+
+export default IssuesPage
